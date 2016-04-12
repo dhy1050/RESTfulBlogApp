b87b40ec509860f1d44d4a24c35a0e47301b009a
Hanying Dong <donghanying1@gmail.com>
2016-04-12 02:37:01 -0400

    little error fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 //app config
 mongoose.connect("mongodb://localhost/restful_blog_app");
-app.set("view engin", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
