ffc8797409e997f5d5a3574e6280fa5eea37804b
Hanying Dong <donghanying1@gmail.com>
2016-04-12 01:43:29 -0400

    add files

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var app = express();
+
+//app config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engin", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//mongoose/model config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type:Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+app.get("/", function(req,res){
+	res.redirect("/blogs");
+});
+
+
+//INDEX ROUTE
+app.get("/blogs", function(req,res){
+	Blog.find({}, function(err,blogs){
+		if(err){
+			console.log("there is error!!");
+		}else{
+			res.render("index",{blogs: blogs});
+		}
+	});
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req,res){
+	res.render("new");
+});
+
+
+//CREATE ROUTE
+app.post("/blogs", function(req,res){
+	//create bloc
+	Blog.create(req.body.blog, function(err,newBlog){
+		if(err){
+			res.render("new");
+		}else{
+			//then, redirect to the index
+			res.redirect("/blogs");
+		}
+	});
+
+});
+
+app.listen(3000,function(){
+	console.log("server is running!");
+});
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+// 	console.log("server is running");
+// });
